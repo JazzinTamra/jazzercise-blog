761bb7a7a51a4f12c89646d39dc905d4f0bc78e2
Tamra Fenstermaker <fenstermaker505@gmail.com>
2015-10-14 15:59:28 -0600

    Added text to Persona 1. Needs spelling edits.

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <!DOCTYPE html>
-<html>
+	<html lang="en">
 	<head>
-		<title></title>
+		<meta charset="UTF-8"/>
+		<title>Jazzercise Blog Persona & Use Case</title>
 	</head>
 	<body>
+		<h1>Persona 1</h1>
+		<p>Persona 1 is a young woman age 18-25. She's a is health conscience college student. Exercise relieves stress for her, which she needs because of her crazy busy days filled with school and with her part-time job. She makes time for Jazzercise because she likes the hard workouts that keep her fit and looking good. Persona 1 finds herself using the Jazzercise site on her phone daily when she plans her workout. She attends classes at different times at different centers, just so she can fit a workout in. She likes knowing what format to expect and what instrutor will be instructing the class. Persona 1 sees the blog topics on the home page when visiting the site, and clicks through to read when the topics are of interest. See uses the tips and sometime talks with her friend before class about the topics she reads about.</p>
+		<h1>Use Case for Persona 1</h1>
+		<p>Persona 1 is checking out the possible class times for tomorrows workout and sees the blog titlesd "Hairstyles for Hitting the Gym." She immedately clicks through since she is growing out her bangs and is finding alturnatives to her current hairstyle. None of the pictures seem like options, but Monica has posted an idea that whe just might try tomorrow. </p>
+		<h2>Persona 2</h2>
+		<p>Persona 2 is a stay at home mother of a 2 year old. She is looking for a community of adults and attends the 9a.m. classes. She wants to exercise feel energetic and so she's able to keep up with her busy toddler. </p>
 	</body>
 </html>

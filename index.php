bfd0323a5f0e0e22a6ab9693840df1c0cbe60b3b
Tamra Fenstermaker <fenstermaker505@gmail.com>
2015-10-15 08:31:55 -0600

    Added list to use case.

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
 <!DOCTYPE html>
-	<html lang="en">
+<html lang="en">
 	<head>
 		<meta charset="UTF-8"/>
 		<title>Jazzercise Blog Persona & Use Case</title>
 	</head>
 	<body>
-		<h1>Persona 1</h1>
-		<p>Persona 1 is a young woman age 18-25. She's a is health conscience college student. Exercise relieves stress for her, which she needs because of her crazy busy days filled with school and with her part-time job. She makes time for Jazzercise because she likes the hard workouts that keep her fit and looking good. Persona 1 finds herself using the Jazzercise site on her phone daily when she plans her workout. She attends classes at different times at different centers, just so she can fit a workout in. She likes knowing what format to expect and what instrutor will be instructing the class. Persona 1 sees the blog topics on the home page when visiting the site, and clicks through to read when the topics are of interest. See uses the tips and sometime talks with her friend before class about the topics she reads about.</p>
-		<h1>Use Case for Persona 1</h1>
-		<p>Persona 1 is checking out the possible class times for tomorrows workout and sees the blog titlesd "Hairstyles for Hitting the Gym." She immedately clicks through since she is growing out her bangs and is finding alturnatives to her current hairstyle. None of the pictures seem like options, but Monica has posted an idea that whe just might try tomorrow. </p>
-		<h2>Persona 2</h2>
-		<p>Persona 2 is a stay at home mother of a 2 year old. She is looking for a community of adults and attends the 9a.m. classes. She wants to exercise feel energetic and so she's able to keep up with her busy toddler. </p>
+		<h1>Rebecca</h1>
+		<p>Rebecca is a young woman age 19. She&rsquo;s a is health conscience college student, and uses exercise to relieve stress. She has a crazy busy life filled with with school and a part-time job. She makes time for Jazzercise because she likes the hard workouts that keep her fit and looking good. Rebecca finds herself using the Jazzercise.com website on her Android phone with Kitkat daily when she plans her workout. Rebecca knows her way around the web and social media, but is not especially technical. She won't be jail breaking her phone anytime soon. She attends classes at different times at different centers all over town, just so she can fit a workout in. She likes knowing what format to expect and what instructor will be teaching the class. Rebecca sees the blog topics on the Jazzercise home page when she visits the site, and clicks through to read when the topics are of interest. See uses the tips and sometime talks with her friend before class about the topics she reads about.</p>
+		<h1>Use Case for Rebecca</h1>
+		<p>Rebecca is checking out the possible class times for tomorrow&rsquo;s workout and sees the blog titled &ldquo;Hairstyles for Hitting the Gym.&rdquo; She immediately clicks through since she is growing out her bangs and is finding alternatives to her current hairstyle. None of the pictures seem like options, but Monica has posted an idea that whe just might try tomorrow.</p>
+		<p>Step for use case: open browser on phone, go to Jazzercise.com, scroll down to blog section, see the Hairstyles for hitting the Gym post, and click the Read More link.</p>
+		<ul>
+			<ol>Action: Open browser and navigate to Jazzercise.com</ol>
+			<ol>Response: Home page loads</ol>
+			<ol>Action: Scrolls down and reads blog post topics</ol>
+			<ol>Action: Clicks VIEW ALL </ol>
+			<ol>Response: Opens www.jazzercise.com/Community/Blog</ol>
+			<ol>Action: Scrolls down to "Hairstyles for Hitting the Gym", Clicks READ MORE</ol>
+			<ol>Response: Opens http://www.jazzercise.com/Community/Blog/September-2015/Hairstyles-for-Hitting-the-Gym</ol>
+		</ul>
+		<h2>Chandra</h2>
+		<p>Chandra is a stay at home mother of a 7 and a 2 year old. She is looking for a community of adults and attends the 9 a.m. class near her house after she gets her daughter off to school. Chandra exercises to feel energetic and so she&rsquo;s able to keep up with her kids. Chandra browses Jazzercise.com weekly on her iPad mini 2 when her 2 yr old is taking a nap. She is very technical and mostly uses her iPad mini for browsing the web and social media. She wants to see what formats her instructors will be doing for the week, and sometimes hits the blog for information on health and fitness topics.</p>
 	</body>
 </html>

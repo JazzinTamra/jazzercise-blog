e584fce47fa6e3eaf63726cc147c9209f172b2a8
Tamra Fenstermaker <fenstermaker505@gmail.com>
2015-10-16 11:44:41 -0600

    Added keys to attributes

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	<body>
 		<h1>Rebecca</h1>
 		<p>Rebecca is a young woman age 19. She&rsquo;s a is health conscience college student, and uses exercise to relieve stress. She has a crazy busy life filled with with school and a part-time job. She makes time for Jazzercise because she likes the hard workouts that keep her fit and looking good. Rebecca finds herself using the Jazzercise.com website on her Android phone with Kitkat daily when she plans her workout. Rebecca knows her way around the web, the Google Drive and social media, but is not especially technical. She won't be jail breaking her phone anytime soon. She attends classes at different times at different centers all over town, just so she can fit a workout in. She likes knowing what format to expect and what instructor will be teaching the class. Rebecca sees the blog topics on the Jazzercise home page when she visits the site, and clicks through to read when the topics are of interest. See uses the tips and sometime talks with her friend before class about the topics she reads about.</p>
-		<h1>Use Case 1 for Rebecca</h1>
+		<h1>Use Case for Rebecca</h1>
 		<p>GOAL: For Rebecca to browse Jazzercise.com blog posts.</p>
 		<p>Rebecca is checking out the possible class times for tomorrow&rsquo;s workout and sees the blog titled &ldquo;Hairstyles for Hitting the Gym.&rdquo; She immediately clicks through since she is growing out her bangs and is finding alternatives to her current hairstyle. None of the pictures seem like options, but Monica has posted an idea in the comments that she just might try tomorrow.</p>
 		<p>Step for the use case: </p>
@@ -28,6 +28,7 @@
 			<li>Title</li>
 			<li>Author</li>
 			<li>Blog content: text and images</li>
+			<li>blogPostId</li>
 		</ul>
 		<h3>Comments Entity</h3>
 		<p>The Comments entity has a many to one relationship with a blog post.</p>
@@ -37,4 +38,6 @@
 			<li>Email</li>
 			<li>Your URL</li>
 			<li>Comments content</li>
+			<li>blogPostId</li>
+			<li>commentId</li>
 		</ul>
